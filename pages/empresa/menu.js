458691d86e9399940a73faf8a0508358b2944b89
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2018-07-19 14:56:38 -0700

    animaciones y css

--- pages/empresa/menu.js
+++ pages/empresa/menu.js
@@ -169,3 +169,17 @@ $(function() {
   });
 })
 
+// determina si un elemento comienza a ser visible
+function isElementVisible(elem){
+   let viewScrollTop = $(window).scrollTop(); // distancia de scroll superior
+   let viewBottom = viewScrollTop + $(window).height(); // distancia de scroll + el alto actual de window (lo no visible por scroll + lo visible)
+   let topElemD = $(elem).offset().top; // distancia desde el elemento hasta el tope superior del viewport
+   return (topElemD < viewBottom);
+}
+
+
+// invoco una función anónima en el evento scroll sobre window
+$(window).on("scroll" ,function() {
+   let elem = $('.iconoSoluciones'); // obtengo el elemento por id
+   isElementVisible(elem) ? elem.addClass('fadeIn') : elem.removeClass('fadeIn'); // si es visible agrego la class, de lo contrario la remuevo
+});

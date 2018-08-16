b22df0faf12e46ad9dd09ba594f721819fc327ec
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2018-08-15 23:43:37 -0700

    link

--- pages/hogar/js/main.js
+++ pages/hogar/js/main.js
@@ -178,19 +178,76 @@ $(window).on("scroll" ,function() {
    isElementVisible(elem) ? elem.addClass('fadeIn') : ""// si es visible agrego la class, de lo contrario la remuevo
 });
 
+
 $(function() {
   $( '#menuMision' ).click(function(){
-    $('#carousel').carousel(0);
-
+      moverCarousel(0);
   });
 
    $( '#menuVision' ).click(function(){
-     $('#carousel').carousel(1);
+      moverCarousel(1);
 
   });
    $( '#menuFilosofia' ).click(function(){
-     $('#carousel').carousel(2);
+      moverCarousel(2);
 
   });
 });
+
+
+
+  function moverCarousel(pos){
+     $('#carousel').carousel(pos);
+    var element = document.getElementById("carousel");
+
+    element.scrollIntoView();
+ }
+  function getGET()
+    {
+        // capturamos la url
+        var loc = document.location.href;
+        // si existe el interrogante
+        if(loc.indexOf('?')>0)
+        {
+            // cogemos la parte de la url que hay despues del interrogante
+            var getString = loc.split('?')[1];
+            // obtenemos un array con cada clave=valor
+            var GET = getString.split('&');
+            var get = {};
+ 
+            // recorremos todo el array de valores
+            for(var i = 0, l = GET.length; i < l; i++){
+                var tmp = GET[i].split('=');
+                get[tmp[0]] = unescape(decodeURI(tmp[1]));
+            }
+            return get;
+        }
+    }
+ 
+    window.onload = function()
+    {
+        // Cogemos los valores pasados por get
+        var valores=getGET();
+        if(valores)
+        {
+            // hacemos un bucle para pasar por cada indice del array de valores
+            for(var index in valores)
+            {
+                console.log("<br>clave: "+index);
+                if(index == "mision"){
+                  moverCarousel(0);
+                }
+                else if(index == "vision"){
+                  moverCarousel(1);
+                }
+                else{
+                  moverCarousel(2);
+                }
+
+            }
+        }else{
+            // no se ha recibido ningun parametro por GET
+            console.log("<br>No se ha recibido ningún parámetro");
+        }
+    }
 

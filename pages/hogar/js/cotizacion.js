bf84c46ee17a61150e67039a7bc01dd0fdfc18b3
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2018-09-28 16:48:01 -0700

    cambios

--- pages/hogar/js/cotizacion.js
+++ pages/hogar/js/cotizacion.js
@@ -5,20 +5,9 @@ function QuitarTexto(){
 
 $(function() {
 
-	$("form").submit(function(evt){
-		evt.preventDefault();
-
-		var data = $(this).serialize();
-
-	    $.post( "http://crm.maxicomm.net/index.php?entryPoint=WebToPersonCapture", data, function( data ) {
-		  console.log( data ); // John
-		}, "json");
-
-		$('.alert').css({display : 'inline-block'});
-	    
-
-	    setTimeout("QuitarTexto()", 4*1000);
-	    
+	$("form").submit(function(){
+		
+		console.log('Se envio el form');
 	    document.getElementById("WebToLeadForm").reset();
 	});
 });

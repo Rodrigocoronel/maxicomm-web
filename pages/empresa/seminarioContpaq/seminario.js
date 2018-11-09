a91a305513894a2477963aa55fceb41536a0e541
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2018-11-08 16:41:58 -0800

    eventos de contpaq

--- pages/empresa/seminarioContpaq/seminario.js
+++ pages/empresa/seminarioContpaq/seminario.js
@@ -2,6 +2,9 @@ function redirect(){
    window.location.href = "https://maxicomm.com.mx/pages/empresa";
 }
 
+var api_production = "https://api.maxicomm.com.mx";
+var api_dev = "http://127.0.0.1:8000";
+
 $(function() {
     $( "#tipo" ).change(function() {
 
@@ -20,9 +23,10 @@ $(function() {
     $("form").submit(function(evt){
     	evt.preventDefault();
     	
-    	let data = $(this).serialize();
+    	let data = $(this).serialize() + '&evento_id=' + eventoSeleccionado.id;
+      data['id_evento'] = eventoSeleccionado.id;
       console.log(data)
-    	$.post( "https://api.maxicomm.com.mx/api/registroSeminario",data, function( response ) {
+    	$.post( api_production+"/api/registroSeminario",data, function( response ) {
 	      console.log(response)
 	      document.getElementById("seminario").reset();
         $('.alert2').removeClass('d-none');
@@ -32,6 +36,38 @@ $(function() {
 	    }), "jsonp";
     });
 
-    
+});
 
+
+  var eventosArray = [];
+  var eventoSeleccionado = [];
+
+function actualizarInfo(){
+   document.getElementById('titulo').innerHTML='<p>'+eventoSeleccionado.titulo+'</p>';
+   document.getElementById('lugar').innerHTML='<p>'+eventoSeleccionado.lugar+'</p>';
+   document.getElementById('horario').innerHTML='<p>'+eventoSeleccionado.horario+'</p>';
+   document.getElementById('expositor').innerHTML='<p>'+eventoSeleccionado.expositor+'</p>';
+   document.getElementById('fecha_evento').innerHTML='<p>'+eventoSeleccionado.fecha_evento+'</p>';
+}
+
+$(function(){
+
+  var $dropdown = $("#selectEvento");
+  $.get(api_production+'/api/evento/listaPorFecha',function(response){
+      let index = 0;
+    $.each(response, function() {
+      $dropdown.append($("<option />").val(index).text(this.titulo));
+      index++;
+    });
+    eventosArray = response;
+    eventoSeleccionado = response[0]
+    actualizarInfo();
+  });
+
+  $( "#selectEvento" ).change(function() {
+    eventoSeleccionado = eventosArray[this.value]
+    actualizarInfo(eventoSeleccionado);
+
+  });
+  
 });
